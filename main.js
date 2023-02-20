e7bbd34c73a3ab4f45f356e7598b64fc8f063bc9
filomenoPonce580 <110742301+filomenoPonce580@users.noreply.github.com>
2023-02-20 15:23:02 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 function solve(str){
  //turn string into array
+  let strArr = str.split('')
   
  //create counts
+  let uppers = 0;
+  let lowers = 0;
+  let nums = 0;
+  let specials = 0;
   
- //
+ //for loop that adds to countds using conditionals
 }
