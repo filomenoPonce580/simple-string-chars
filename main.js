1ddd8d40a684bd05f0b60507bb88bf944f0f702e
filomenoPonce580 <110742301+filomenoPonce580@users.noreply.github.com>
2023-02-21 10:23:07 -0800

    Update main.js
    reworked. success

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 function solve(str){
- //turn string into array
-  let strArr = str.split('')
+//create empty array
+  let final = [];
+
+//create variables corresponding to the characters we are looking for. 
+  let uppers = /[A-Z]/g;
+  let lowers = /[a-z]/g;
+  let nums = /[0-9]/g;
+  let specials = /\W/g;
   
- //create counts
-  let uppers = 0;
-  let lowers = 0;
-  let nums = 0;
-  let specials = 0;
+  //turn into an array to loop through
+  let toMatch = [uppers, lowers, nums, specials]
   
- //for loop that adds to countds using conditionals
-  for(let i = 0; i < strArr.length; i++){
-    
+  //for loop iterates through toMatch array
+  for(let i = 0; i < toMatch.length; i++){
+    //match the values in array to the string, use the .lenth propery of new arrays and push that into final array
+    str.match(toMatch[i]) ? final.push(str.match(toMatch[i]).length) : final.push(0);
   }
   
   //return
+  return final
 }
